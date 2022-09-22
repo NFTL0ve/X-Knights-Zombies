bcfa50e5e221063ed3ebf318a60edd66bc6d5c8c
NFTL0ve <mbrandtk14@gmail.com>
2022-09-22 18:44:06 +0200

    mvp

--- index.js
+++ index.js
@@ -107,27 +107,23 @@ draw() {
 
 //create platforms
 class Platform {
-constructor() {
-
+  constructor(horizontalPosition) {
     this.position = {
-        x: 40,
-        y: 300
-    }
-    this.width = 200
-    this.height = 80
+      x: horizontalPosition,
+      y: 500,
+    };
+    this.width = 200;
+    this.height = 80;
+  }
 
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+  }
 }
-
-draw(){
-    c.fillStyle = 'red'
-    c.fillRect(this.position.x,
-        this.position.y, this.width, this.height)
-}
-update(){
-    this.draw()
-
-
-}}
 
 
 class Projectile {
@@ -151,7 +147,7 @@ this.radius = 7}
 }
 }
 
-class Grid {
+/* class Grid {
     constructor() {
 
         this.position = {
@@ -194,12 +190,25 @@ console.log(this.aliens)
     }
 }
 
-
+*/
 
 const player = new Player()
 const projectiles = []
-const platform = new Platform 
-const grids = [new Grid()]
+const platform = new Platform(20 * 1.2);
+const platform1 = new Platform(400 * 1.2);
+const platform2 = new Platform(900 * 1.2);
+const platform3 = new Platform(1400 * 1.2);
+const platform4 = new Platform(1750 * 1.2);
+const platform5 = new Platform(2100 * 1.2);
+const platform6 = new Platform(2550 * 1.2);
+const platform7 = new Platform(3000 * 1.2);
+const platform8 = new Platform(3500 * 1.2);
+const platform9 = new Platform(3900 * 1.2);
+const platform10 = new Platform(4200 * 1.2);
+
+// const grids = [new Grid()]
+// const myGrid = new Grid();
+// myGrid.aliens  ---> here you prob. have an array of aliens
 
 const keys = {
 
@@ -210,9 +219,9 @@ const keys = {
         pressed: false
         }
 }
-/*
- function init() {
-platformImage = src="img/platform.png"
+
+ /*function init() {
+platformImage = 
 player = new Player ()
 platforms = [
     new Platform({
@@ -228,13 +237,23 @@ platforms = [
     })
 
 ]
-}*/
+} /*/
 //call animate function to constantly call
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
   platform.draw();
+  platform1.draw();
+  platform2.draw();
+  platform3.draw();
+  platform4.draw();
+  platform5.draw();
+  platform6.draw();
+  platform7.draw();
+  platform8.draw();
+  platform9.draw();
+  platform10.draw();
   //projectiles shoot
 
   projectiles.forEach((projectile) => {
@@ -248,24 +267,29 @@ function animate() {
   } else player.velocity.x = 0;
 
   //projectiles kill
-  projectiles.forEach((projectile, i) => {    
+  // projectiles.forEach((projectile, i) => {    
 
-    if (
-      projectile.position.x - projectile.radius <=
-      alien.position.x + alien.width
-    ) {
-        grid.aliens.splice(i, 1);
-        projectiles.splice(i, 1);
-
-    //   setTimeout(() => {
+    // if (
+    //   projectile.position.x - projectile.radius <=
+    //   alien.position.x + alien.width
+    // ) {
     //     grid.aliens.splice(i, 1);
     //     projectiles.splice(i, 1);
-    //   }, 0);
-    }
+
+    // //   setTimeout(() => {
+    // //     grid.aliens.splice(i, 1);
+    // //     projectiles.splice(i, 1);
+    // //   }, 0);
+    // }
 
     
-  });
+
+    
+   
+  // )
+ ;
   //platform Animate
+  
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5;
   } else if (keys.left.pressed && player.position.x > 50) {
@@ -278,6 +302,56 @@ function animate() {
     } else if (keys.left.pressed) {
       platform.position.x += 5;
     }
+    if (keys.right.pressed) {
+        platform1.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform1.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform2.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform2.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform3.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform3.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform4.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform4.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform5.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform5.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform6.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform6.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform7.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform7.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform8.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform8.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform9.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform9.position.x += 5;
+      }
+      if (keys.right.pressed) {
+        platform10.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform10.position.x += 5;
+      }
   }
 
   if (
@@ -287,17 +361,109 @@ function animate() {
     player.position.x + player.width >= platform.position.x &&
     player.position.x <= platform.position.x + platform.width
   ) {
-    player.velocity.y = 0;
+    player.velocity.y = -12;
   }
-  //Grids of Aliens moving
+  if (
+    player.position.y + player.height <= platform1.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform1.position.y &&
+    player.position.x + player.width >= platform1.position.x &&
+    player.position.x <= platform1.position.x + platform1.width
+  ) {
+    player.velocity.y = -16;
+    player.velocity.x = -10;
+  }
+  if (
+    player.position.y + player.height <= platform2.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform2.position.y &&
+    player.position.x + player.width >= platform2.position.x &&
+    player.position.x <= platform2.position.x + platform2.width
+  ) {
+    player.velocity.y = -16;
+  }
+  if (
+    player.position.y + player.height <= platform3.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform3.position.y &&
+    player.position.x + player.width >= platform3.position.x &&
+    player.position.x <= platform3.position.x + platform3.width
+  ) {
+    player.velocity.y = -10.5;
+  }
+  if (
+    player.position.y + player.height <= platform4.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform4.position.y &&
+    player.position.x + player.width >= platform4.position.x &&
+    player.position.x <= platform4.position.x + platform4.width
+  ) {
+    player.velocity.y = -9;
+  }
+  if (
+    player.position.y + player.height <= platform5.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform5.position.y &&
+    player.position.x + player.width >= platform5.position.x &&
+    player.position.x <= platform5.position.x + platform5.width
+  ) {
+    player.velocity.y = -14;
+  }
+  if (
+    player.position.y + player.height <= platform6.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform6.position.y &&
+    player.position.x + player.width >= platform6.position.x &&
+    player.position.x <= platform6.position.x + platform6.width
+  ) {
+    player.velocity.y = -13;
+
+  }
+  if (
+    player.position.y + player.height <= platform7.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform7.position.y &&
+    player.position.x + player.width >= platform7.position.x &&
+    player.position.x <= platform7.position.x + platform7.width
+  ) {
+    player.velocity.y = -15;
+  }
+  if (
+    player.position.y + player.height <= platform8.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform8.position.y &&
+    player.position.x + player.width >= platform8.position.x &&
+    player.position.x <= platform8.position.x + platform8.width
+  ) {
+    player.velocity.y = -12;
+  }
+  if (
+    player.position.y + player.height <= platform9.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform9.position.y &&
+    player.position.x + player.width >= platform9.position.x &&
+    player.position.x <= platform9.position.x + platform9.width
+  ) {
+    player.velocity.y = -12;
+  }
+  if (
+    player.position.y + player.height <= platform10.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform10.position.y &&
+    player.position.x + player.width >= platform10.position.x &&
+    player.position.x <= platform10.position.x + platform10.width
+  ) {
+    player.velocity.y = -1;
+  }
+ /* //Grids of Aliens moving
   grids.forEach((grid) => {
     grid.update();
     grid.aliens.forEach((alien, i) => {
       alien.update({ velocity: grid.velocity });
     });
-  });
+  }); */
+// }
 }
-
 animate()
 //Select keys when they are pressed§
 addEventListener('keydown', ({ keyCode }) => {
@@ -358,7 +524,7 @@ addEventListener('keyup', ({ keyCode }) => {
             break
         case 38:
              console.log('up')
-             player.velocity.y -= 4.5
+             player.velocity.y -= .01
             break
 
         case 40:
